c8fc099694a00ca62230259fb6b6ab1f4116330a
KawaiiWafu <remiliass@gmail.com>
2021-08-16 17:21:12 +0200

    Changed foreign keys to match models

--- data/order.go
+++ data/order.go
@@ -1,7 +1,7 @@
 package data
 
 type Order struct {
-	ID       uint64 `gorm:"primaryKey"`
-	Items    []OrderItem
-	Customer Customer
+	ID       uint64      `gorm:"primaryKey"`
+	Items    []OrderItem `gorm:"foreignKey:ID"`
+	Customer Customer    `gorm:"foreignKey:ID"`
 }
