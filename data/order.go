6d8bc1c6e3e3c4ac5e021166594e63de118c3d6a
KawaiiWafu <remiliass@gmail.com>
2021-08-17 22:09:50 +0200

    Added creating product

--- data/order.go
+++ data/order.go
@@ -1,7 +1,8 @@
 package data
 
 type Order struct {
-	ID       uint64      `gorm:"primaryKey"`
-	Items    []OrderItem `gorm:"foreignKey:ID"`
-	Customer Customer    `gorm:"foreignKey:ID"`
+	ID        uint64      `gorm:"primaryKey"`
+	Items     []OrderItem `gorm:"foreignKey:ID"`
+	Customer  Customer    `gorm:"foreignKey:ID"`
+	Confirmed bool        `gorm:"default:false"`
 }
--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/KawaiiWafu/apitask/data"
 	"github.com/gofiber/fiber/v2"
@@ -42,6 +43,7 @@ func main() {
 	if args[0] == "start" {
 		app := fiber.New()
 		app.Get("/customer/new/:name/:email", createCustomer)
+		app.Get("/product/new/:name/:price", createProduct)
 		app.Listen(":3000")
 	}
 }
@@ -52,6 +54,23 @@ func createCustomer(c *fiber.Ctx) error {
 		Email: c.Params("email"),
 	}
 	result := db.Create(&customer)
+	if result.Error != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	} else {
+		return c.SendStatus(fiber.StatusCreated)
+	}
+}
+
+func createProduct(c *fiber.Ctx) error {
+	price, err := strconv.ParseFloat(c.Params("price"), 64)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	product := data.Product{
+		Name:     c.Params("name"),
+		PriceCZK: price,
+	}
+	result := db.Create(&product)
 	if result.Error != nil {
 		return c.SendStatus(fiber.StatusBadRequest)
 	} else {
